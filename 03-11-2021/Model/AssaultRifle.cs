d7546ae99d5c6102f28bbee536ee72e18e5f204b
CavidH <hcavid386@gmail.com>
2021-11-03 11:14:57 +0400

    Add project files.

---
+++ 03-11-2021/Model/AssaultRifle.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace _03_11_2021.Model
+{
+    class AssaultRifle : Gun
+    {
+        public int AmountOfBullets { get; set; }
+        public int MagazineCapacity { get; set; }
+        public int CurrentMagazineCount { get; set; }
+
+        public AssaultRifle(string model,  double power, int amountOfBullets, int magazineCapacity, int currentMagazineCount) : base(model, power)
+        {
+            AmountOfBullets = amountOfBullets;
+            MagazineCapacity = magazineCapacity;
+            CurrentMagazineCount = currentMagazineCount;
+        }
+        #region Reload1
+
+        public string Reload()
+        {
+            int Empty = MagazineCapacity - CurrentMagazineCount;
+            if ((AmountOfBullets - Empty) > 0)
+            {
+                AmountOfBullets -= Empty;
+                CurrentMagazineCount += Empty;
+                return $"{Empty} gulle elave olundu";
+
+            }
+            else
+            {
+                CurrentMagazineCount += AmountOfBullets;
+                AmountOfBullets = 0;
+                return "yerde qalan gulle  magazine elave olundu ";
+            }
+
+        }
+        #endregion
+        #region Reload2
+        public string Reload(int count)
+        {
+            if (count+CurrentMagazineCount>MagazineCapacity)
+            {
+                return $"Maghazinde {MagazineCapacity - CurrentMagazineCount} " +
+                    $"yer var siz {count} gulle doldura bilmezsiz! " +
+                    $" magazinni fulladiq ";
+                Reload();
+
+            }
+            else if (AmountOfBullets<count)
+            {
+                return $"sizin yeteri qeder gulleniz yoxdu \n" +
+                    $"hal hazirda qalan gulleniz:{AmountOfBullets} bu " +
+                    $"qederdi ve bu gulleni magazine doldurduq";
+                Reload();
+            }
+            else
+            {
+                AmountOfBullets -= count;
+                CurrentMagazineCount += count;
+                return $"{count} gulle dolduruldu";
+
+            }
+
+            }
+        #endregion
+        #region Shoot1
+        public string Shoot()
+        {
+            for (int i = CurrentMagazineCount - 1; i >= 0; i--)
+            {
+                Thread.Sleep(2000);
+                return "   ->   ";
+            }
+            return "Gulle bitdi Reloada ehtiyac var";
+        }
+        #endregion
+        #region Shoot2
+        public string Shoot(int Count)
+        {
+            CurrentMagazineCount -= 1;
+            return "   ->   ";
+        }
+        #endregion
+
+    }
+}
---
+++ 03-11-2021/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using _03_11_2021.Model;
+
+namespace _03_11_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string GunNAme=PrintAndReturn("Salam Silahinizin modelin" +
+                " qeyd edin zehmet olmasa:");
+           int Power 
+
+
+            AssaultRifle assaultRifle=new AssaultRifle()
+
+
+
+
+
+        }
+
+        public static string PrintAndReturn(string output)
+        {
+            Console.WriteLine(output);
+            return Console.ReadLine();
+        }
+        public static int PrintAndReturnI(string output)
+        {
+            Console.WriteLine(output);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
